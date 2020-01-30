99ba720d25e721bd79b57dc272f8e51803d2cb77
bibixx <bartosz@legiec.io>
2020-01-30 21:03:30 +0100

    Initial MVP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from 'antd';
+import SubjectAllQuestions from './SubjectAllQuestions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Footer } = Layout;
+
+const App = () => (
+  <Layout className="layout">
+    <SubjectAllQuestions />
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+  </Layout>
+);
 
 export default App;
