a09cbd0929b1a0813686fb2a9e6697ae8b80ac3b
vvooycik <maciek.wojcik22@gmail.com>
2020-09-22 16:59:05 +0200

    Fix the add-subject-endpoint

--- backend/subjects/subject.service.ts
+++ backend/subjects/subject.service.ts
@@ -4,13 +4,12 @@ import { Subject } from '../types/subject';
 import writeSubject from './subject.model';
 
 const addNewSubject = (res: NowResponse, title: string, id: string): void => {
-  const dbName = `../../public/data/${id}.json`;
   const data: Question[] = [];
   const newSubject: Subject = {
     title,
     id,
     data,
   };
-  writeSubject(newSubject, res, dbName);
+  writeSubject(newSubject, res, id);
 };
 export default addNewSubject;
