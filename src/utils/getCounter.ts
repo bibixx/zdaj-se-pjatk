62afd6c217a3b2eac048641703f3b5e820df810f
alszczep <al.szczepura@gmail.com>
2023-05-20 19:03:25 +0200

    Ensure question id consistency

--- src/utils/getCounter.ts
+++
@@ -1,6 +0,0 @@
-export const getCounter = () => {
-  let counter = 0;
-
-  // eslint-disable-next-line no-plusplus
-  return () => counter++;
-};
--- src/validators/subjects.ts
+++ src/validators/subjects.ts
@@ -1,6 +1,5 @@
 import * as yup from 'yup';
 import { transformNull } from 'utils/transformNull';
-import { getCounter } from 'utils/getCounter';
 
 const answerSchema = yup.object({}).shape({
   answer: yup.string().ensure(),
@@ -20,51 +19,74 @@ const commentSchema = yup.object({}).shape({
   isMarkdown: yup.boolean().default(false),
 });
 
-const questionSchemaEmptyIdCounter = getCounter();
-
 const questionSchemaShape = {
   question: yup.string().ensure(),
-  id: yup
-    .string()
-    .transform(transformNull)
-    .default(() => `empty-${questionSchemaEmptyIdCounter()}`),
+  id: yup.string().transform(transformNull),
   isMarkdown: yup.boolean().default(false),
   comments: yup.array().of(commentSchema).nullable(true).ensure(),
   answers: yup.array().of(answerSchema).required(),
   overwritten: yup.boolean().nullable(),
   added: yup.boolean().nullable(),
 };
-
-const questionSchema = yup.object({}).shape(questionSchemaShape);
+const nullableIdQuestionSchema = yup.object({}).shape(questionSchemaShape);
+const questionSchema = yup.object({}).shape({
+  ...questionSchemaShape,
+  id: yup.string().transform(transformNull).ensure(),
+});
 
 const overrideQuestionSchemaShape = {
   ...questionSchemaShape,
   answers: yup.array().of(answerSchema).ensure(),
 };
-
-const overrideQuestionSchema = yup
+const nullableIdOverrideQuestionSchema = yup
   .object({})
   .shape(overrideQuestionSchemaShape);
+const overrideQuestionSchema = yup.object({}).shape({
+  ...overrideQuestionSchemaShape,
+  id: yup.string().transform(transformNull).ensure(),
+});
 
 const subjectSchemaShape = {
   id: yup.string().nullable().required(),
   title: yup.string().ensure(),
-  data: yup.array().of(questionSchema).required(),
+  data: yup.array().of(nullableIdQuestionSchema).required(),
   updatedAt: yup.number().required(),
 };
-export const subjectSchema = yup.object({}).shape(subjectSchemaShape);
+export const nullableQuestionIdSubjectSchema = yup
+  .object({})
+  .shape(subjectSchemaShape);
+export const subjectSchema = yup.object({}).shape({
+  ...subjectSchemaShape,
+  data: yup.array().of(questionSchema).required(),
+});
 
 const overrideSubjectSchemaShape = {
   ...subjectSchemaShape,
   id: yup.string(),
   title: yup.string(),
-  data: yup.array().of(overrideQuestionSchema).required(),
+  data: yup.array().of(nullableIdOverrideQuestionSchema).required(),
 };
-export const overrideSubjectSchema = yup
+export const nullableQuestionIdOverrideSubjectSchema = yup
   .object({})
   .shape(overrideSubjectSchemaShape);
+export const overrideSubjectSchema = yup.object({}).shape({
+  ...overrideSubjectSchemaShape,
+  data: yup.array().of(overrideQuestionSchema).required(),
+});
 
 export const learntQuestionSchema = yup.array().of(yup.string().required());
+
+export type NullableIdQuestion = yup.Asserts<typeof nullableIdQuestionSchema>;
+export type NullableIdQuestionSubject = yup.Asserts<
+  typeof nullableQuestionIdSubjectSchema
+>;
+
+export type NullableIdOverrideQuestion = yup.Asserts<
+  typeof nullableIdOverrideQuestionSchema
+>;
+export type NullableIdQuestionOverrideSubject = yup.Asserts<
+  typeof nullableQuestionIdOverrideSubjectSchema
+>;
 
 export type Answer = yup.Asserts<typeof answerSchema>;
 export type Comment = yup.Asserts<typeof commentSchema>;
