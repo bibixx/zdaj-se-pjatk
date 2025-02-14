1448912eb1e1be17801c5c56e0bd018ec532133e
Bartosz Legięć <bartosz@legiec.io>
2025-02-14 15:24:29 +0100

    Disable session replays

--- src/utils/sentry/main.ts
+++ src/utils/sentry/main.ts
@@ -9,8 +9,8 @@ Sentry.init({
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
   tracePropagationTargets: ['localhost', /^https:\/\/zdaj\.se/, /^https:\/\/zdaj-se\.vercel\.app/],
   // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  replaysSessionSampleRate: 0,
+  replaysOnErrorSampleRate: 0,
   integrations: [
     Sentry.reactRouterV7BrowserTracingIntegration({
       useEffect: useEffect,
