fe713e3c01e38d1539685edfd49151d6bea57043
Bartosz Legięć <bartosz@legiec.io>
2025-02-14 15:26:41 +0100

    Disable whole integration

--- src/utils/sentry/main.ts
+++ src/utils/sentry/main.ts
@@ -1,24 +1,21 @@
-import * as Sentry from '@sentry/react';
+import { init, reactRouterV7BrowserTracingIntegration } from '@sentry/react';
 import { useEffect } from 'react';
 import { useLocation, useNavigationType, createRoutesFromChildren, matchRoutes } from 'react-router';
 
-Sentry.init({
+init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
   tracePropagationTargets: ['localhost', /^https:\/\/zdaj\.se/, /^https:\/\/zdaj-se\.vercel\.app/],
   // Session Replay
-  replaysSessionSampleRate: 0,
-  replaysOnErrorSampleRate: 0,
   integrations: [
-    Sentry.reactRouterV7BrowserTracingIntegration({
+    reactRouterV7BrowserTracingIntegration({
       useEffect: useEffect,
       useLocation,
       useNavigationType,
       createRoutesFromChildren,
       matchRoutes,
     }),
-    Sentry.replayIntegration(),
   ],
 });
