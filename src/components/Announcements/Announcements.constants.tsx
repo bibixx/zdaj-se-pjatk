822845fcbab04abdc5bacc700710a53def8b025b
Bartosz Legięć <bartosz@legiec.io>
2025-03-07 00:56:18 +0100

    Remove zdaj-se files disclaimer

--- src/components/Announcements/Announcements.constants.tsx
+++ src/components/Announcements/Announcements.constants.tsx
@@ -2,19 +2,4 @@ import { Toast } from 'components/ui/use-toast';
 
 export const CLOSED_ANNOUNCEMENTS_KEY = 'closed-announcements';
 
-export const ANNOUNCEMENTS: Toast[] = [
-  {
-    htmlId: 'mykhi-closing-resolved',
-    title: 'Zamknięcie mykhi 20 lutego',
-    description: (
-      <span>
-        Wszystkie pliki dostępne są na{' '}
-        <a href="https://github.com/bibixx/zdaj-se-pjatk-files" target="_blank" className="underline" rel="noreferrer">
-          github.com/bibixx/zdaj-se-pjatk-files
-        </a>
-        . W ciągu najbliższych dni powinny się one też pojawić bezpośrednio na stronie zdaj.se.
-      </span>
-    ),
-    variant: 'blue',
-  },
-];
+export const ANNOUNCEMENTS: Toast[] = [];
