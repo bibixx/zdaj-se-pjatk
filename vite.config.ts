35b86e9dfeaf5d3d7f9ee6e4318ab0e133f7ab51
Bartosz Legięć <bartosz@legiec.io>
2025-12-27 18:07:41 +0100

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import posthogRollupPlugin from '@posthog/rollup-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
@@ -16,29 +15,39 @@ const alias = fs
   }));
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(async () => {
+  const plugins = [
     inlineHeadPlugin({
       scriptPath: './src/head.ts',
     }),
     react(),
-    posthogRollupPlugin({
-      personalApiKey: process.env.POSTHOG_PERSONAL_API_KEY!,
-      envId: process.env.VITE_PUBLIC_POSTHOG_KEY!,
-      host: process.env.VITE_PUBLIC_POSTHOG_HOST,
-      sourcemaps: {
-        deleteAfterUpload: true,
-      },
-    }),
-  ],
-  base: '/',
-  resolve: {
-    alias,
-  },
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-  },
-  build: {
-    sourcemap: true,
-  },
+  ];
+
+  if (process.env.POSTHOG_PERSONAL_API_KEY && process.env.VITE_PUBLIC_POSTHOG_KEY) {
+    const { default: posthogRollupPlugin } = await import('@posthog/rollup-plugin');
+    plugins.push(
+      posthogRollupPlugin({
+        personalApiKey: process.env.POSTHOG_PERSONAL_API_KEY,
+        envId: process.env.VITE_PUBLIC_POSTHOG_KEY,
+        host: process.env.VITE_PUBLIC_POSTHOG_HOST,
+        sourcemaps: {
+          deleteAfterUpload: true,
+        },
+      }),
+    );
+  }
+
+  return {
+    plugins,
+    base: '/',
+    resolve: {
+      alias,
+    },
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    },
+    build: {
+      sourcemap: true,
+    },
+  };
 });
